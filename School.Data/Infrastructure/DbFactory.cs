647af9478de5ba9e1e2dd20070afcf1f171aae5c
Sarvabowmen <ramkihardy@gmail.com>
2017-04-26 12:44:46 +0530

    Adding Repository classes

---
+++ School.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory 
+    {
+        SchoolDbContext schoolContext;
+
+        public SchoolDbContext Init()
+        {
+            return schoolContext ?? (schoolContext = new SchoolDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (schoolContext != null)
+                schoolContext.Dispose();
+        }
+    }
+}
---
+++ School.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private SchoolDbContext schoolContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public SchoolDbContext SchoolDbContext
+        {
+            get
+            {
+                return schoolContext ?? (schoolContext = dbFactory.Init());
+            }
+        }
+
+        public void Commit() {
+
+            SchoolDbContext.Commit();
+        }
+    }
+}
